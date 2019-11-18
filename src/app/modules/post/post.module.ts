90a318525927b639c4a46e5332c32b1b5c9b46d0
shipingzhong <shipingzhong@gmail.com>
2019-11-19 00:07:32 +0800

    11-18 home

--- src/app/modules/post/post.module.ts
+++ src/app/modules/post/post.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post.component';
-
-
+import { AppRoutingModule } from '../../app-routing.module';
+import { PostItemComponent } from './components/post-item/post-item.component';
 
 @NgModule({
-  declarations: [PostComponent],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    PostComponent
-  ]
+  declarations: [PostComponent, PostItemComponent],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [PostComponent],
 })
-export class PostModule { }
+export class PostModule {}
