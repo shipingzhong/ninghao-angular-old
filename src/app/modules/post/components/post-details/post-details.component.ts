90587c88ee0c1a864a9ec458f66dec6e20ad16f3
shipingzhong <shipingzhong@gmail.com>
2019-11-21 00:17:59 +0800

    2019-11-21 0:17 commit

--- src/app/modules/post/components/post-details/post-details.component.ts
+++ src/app/modules/post/components/post-details/post-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
+  
   ngOnInit() {
+    this.route.paramMap.subscribe(
+      params=>{
+        const postId = +params.get('id');
+        console.log(postId);
+      }
+    )
   }
 
 }
